7ee94304184540004f9bf9194b9ed0199acc64d0
Susannah <susannah.tysor@gmail.com>
2016-07-20 11:55:31 -0700

    Graphed meetings attendance

---
+++ MeetingsAttendance.R
@@ -0,0 +1,29 @@
+# Make meeting attendance graph
+#
+
+library(ggplot2)
+library(ggrepel)
+library(viridis)
+source('leafletmap.R')
+
+file <- "Full Annual Meetings Table The Ecological Society of Americas History and Records.csv"
+citydat <- read.csv(file, stringsAsFactors=FALSE)
+
+attendance <- citydat[which(citydat$Attendance > 0),]
+city_colors <- viridis_pal()(length(unique(attendance$Location)))
+
+ggplot(attendance, aes(Year, Attendance)) +
+    geom_line() +
+    geom_point(aes(color = Location), size = 5) +
+    geom_text_repel(aes(label=Location, color=Location), size = 10) +
+    #labels
+    ggtitle("ESA Annual Meeting Attendance") +
+    ylab("Number of Registered Attendees") +
+    #aesthetics
+    theme_bw(base_size = 50) +
+    theme(
+          axis.title.x = element_blank(),
+          legend.position = "none") +
+    scale_colour_manual(values=city_colors)
+
+
