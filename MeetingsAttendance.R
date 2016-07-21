0a16d3c75941d96ae39890a0ae66fcf34b0be60b
Susannah <susannah.tysor@gmail.com>
2016-07-21 16:13:21 -0700

    Changed meetings attendance graph to a svg

--- MeetingsAttendance.R
+++ MeetingsAttendance.R
@@ -4,26 +4,27 @@
 library(ggplot2)
 library(ggrepel)
 library(viridis)
-source('leafletmap.R')
+#source('leafletmap.R')
 
 file <- "Full Annual Meetings Table The Ecological Society of Americas History and Records.csv"
-citydat <- read.csv(file, stringsAsFactors=FALSE)
+citydat <- read.csv(file, stringsAsFactors = FALSE)
 
 attendance <- citydat[which(citydat$Attendance > 0),]
 city_colors <- viridis_pal()(length(unique(attendance$Location)))
 
-ggplot(attendance, aes(Year, Attendance)) +
+attendplot <- ggplot(attendance, aes(Year, Attendance)) +
     geom_line() +
     geom_point(aes(color = Location), size = 5) +
-    geom_text_repel(aes(label=Location, color=Location), size = 10) +
+    geom_text_repel(aes(label = Location, color = Location), size = 5) +
     #labels
     ggtitle("ESA Annual Meeting Attendance") +
     ylab("Number of Registered Attendees") +
     #aesthetics
-    theme_bw(base_size = 50) +
+    theme_bw(base_size = 20) +
     theme(
           axis.title.x = element_blank(),
           legend.position = "none") +
-    scale_colour_manual(values=city_colors)
+    scale_colour_manual(values = city_colors)
 
 
+ggsave(file = "MeetingAttendance.svg", plot = attendplot, width=15, height=10)
